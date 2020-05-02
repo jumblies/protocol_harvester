39232f9ce652093619cea7dc986b6db568573295
jumblies <glamke@gmail.com>
2020-05-02 12:16:53 -0400

    Add files via upload

--- rename_csv.py
+++ rename_csv.py
@@ -13,9 +13,22 @@ def csv_renamer(filename):
         
         print(new_file_name)
 
+<<<<<<< HEAD
+        sanitized_new_file_name = "".join(x for x in new_file_name if x.isalnum())
+        
+        # print(sanitized_new_file_name)
+        #copy csv to new filename
+        
+        shutil.copy(filename, (sanitized_new_file_name + ".csv"))
+
+
+        shutil.copy(filename[0:-4]+".pdf", (sanitized_new_file_name + ".pdf"))
+
+=======
         sanitized_new_file_name = "".join(x for x in new_file_name if x.isalnum()) + ".csv"
         print(sanitized_new_file_name)
         shutil.copy(filename, (sanitized_new_file_name))
+>>>>>>> 36de66dd1396d7620c4cb6989489d46c2f91faa4
 
 # csv_renamer(myfile)
 
